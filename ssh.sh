6208fa5acb37a450986aa870f5af3751ab13bad2
Alexandru <alexandru.dumitru@ejobsgroup.ro>
2018-04-10 15:46:27 +0300

    Adding colors to the outputs

--- ssh.sh
+++ ssh.sh
@@ -3,14 +3,18 @@
 # Find SSH key and show it
 # If not existing, generate a new SSH key
 
+GREEN='\033[0;32m'
+WHITE='\033[0m'
+RED='\033[0;31m'
+
 if [ "$(find ~/.ssh/id_rsa.pub | cat ~/.ssh/id_rsa.pub | grep 'ssh-rsa')" ];
 then
-   echo "Your ssh key is: "
-   echo "================="
+   echo "${GREEN}Your ssh key is: "
+   echo "=================${WHITE}"
    cat ~/.ssh/id_rsa.pub
 else
-   echo "SSH key not found. Generate one now."	
-   echo "Enter your email address: "
+   echo "${RED}SSH key not found. Generate one now."	
+   echo "${WHITE}Enter your email address: "
    read email
    ssh-keygen -t rsa -C "$email"  
 fi
